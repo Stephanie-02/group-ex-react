0b0b5149b16596c0fe7f39da5fb3cdfc4f47fcac
Stephanie Achieng <stephanieachieng02@gmail.com>
2023-05-29 14:38:26 +0300

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import CardComponent from "./CardComponent.jsx";
 
 function App() {
+
+  const [ items, setItems ] = useState([]);
+  const [ inputText, setInputText ] = useState("");
+
   return (
     <div>
-      <CardComponent
-        content={
-          "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam."
-        }
+      <input onChange = {(ev) => {
+        let text = ev.target.value;
+        setInputText(text);
+      }}type="text"></input>
+      <button onClick = {() => {
+        let newItem = [...items, inputText];
+        setItems(newItem);
+      }}>Add Item</button>
+      {items.map ((item) => {
+        return <CardComponent
+        content={item}
       ></CardComponent>
+      })}
+     
     </div>
   );
 }
