8ab4cbb448bf3e6e150cc4363fceec9e1cdedb7a
Stephanie Achieng <stephanieachieng02@gmail.com>
2023-06-04 08:36:40 +0300

    Done

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Age } from './Age';
 
 export class Welcome extends React.Component {
     render () {
         return (
             <div>
                 <p> Welcome, <strong>{this.props.name}</strong>!</p>
-                <p>Your age is {this.props.age}</p>
+                <Age age={this.props.age}/>
             </div>
         )
 
