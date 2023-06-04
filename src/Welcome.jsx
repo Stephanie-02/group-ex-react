f6f879ddf2f7d38b842a74cb151c5643ea99d1fa
Stephanie Achieng <stephanieachieng02@gmail.com>
2023-06-04 12:45:10 +0300

    Done

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -7,7 +7,7 @@ export class Welcome extends React.Component {
         return (
             <div>
                 <p> Welcome, <strong>{name}</strong>!</p>
-                {age && <Age age={age} />}
+                {age > 18 && age < 65 && <Age age={age} />}
             </div>
         )
 
