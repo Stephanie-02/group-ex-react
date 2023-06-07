a20a84745063132d174e5f1c2b0ae4b810513f2f
Stephanie Achieng <stephanieachieng02@gmail.com>
2023-06-07 15:08:13 +0300

    Done

---
+++ src/LoginComponent.jsx
@@ -0,0 +1,13 @@
+export default function LoginComponent ({visible, setLoggedIn, setUsername}) {
+    return (
+        <div className={visible ? "" : "hidden"}>
+            <h3>Login</h3>
+            <input id="username" type="text"></input>
+            <button onClick = {() => {
+                let text = document.querySelector("#username").value;
+                setUsername(text);
+                setLoggedIn(true);
+            }}>Log In</button>
+        </div>
+    );
+}
