e39fac306a4be015f92c37993de6dfa2b9b485ea
Stephanie Achieng <stephanieachieng02@gmail.com>
2023-06-19 14:32:57 +0300

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import { Routes, Route, Link } from 'react-router-dom';
 
 import NumberDisplay from './NumberDisplay';
+import CardComponent from "./CardComponent";
+import Welcome from './Welcome';
+import NotFound from './NotFound';
 
 function App() {
   return (
     <div>
-      <NumberDisplay></NumberDisplay>
+      <Routes>
+        <Route path='/card' element={<CardComponent></CardComponent>}></Route>
+        <Route path='/number' element={<NumberDisplay></NumberDisplay>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+        <Route path='/' element={<Welcome></Welcome>}></Route>
+      </Routes>
+      <br />
+      <Link to={'/'}>Home</Link>
+      <br />
+      <Link to={'/card'}>Card</Link>
+      <br />
+      <Link to={'/number'}>Number</Link>
+      {/* <br />
+      <Link to={'*'}>404 Not Found</Link> */}
     </div>
   );
 }
